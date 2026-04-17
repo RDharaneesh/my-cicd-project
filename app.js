663c29bb885775596fa95506b032876201d6420a
RDharaneesh <155134632+RDharaneesh@users.noreply.github.com>
2026-04-17 18:31:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = 3000;
 
+// Serve static files
+app.use(express.static(__dirname));
+
+// Route to load index.html
 app.get('/', (req, res) => {
-    res.send('🚀 Microservice App is Running!');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Health check (for Kubernetes/Prometheus)
 app.get('/health', (req, res) => {
     res.json({ status: 'UP' });
 });
